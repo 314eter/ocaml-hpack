10e72bed188070551a03e3993d8138dbcd9d1bd7
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2019-04-04 13:19:43 +0200

    Simplify hpack-test-case

--- test/hpack_test_case.ml
+++ test/hpack_test_case.ml
@@ -1,43 +1,47 @@
 open Hpack
 
+module Json = Yojson.Basic.Util
+
+module Hex = struct
+  let of_string s = Hex.show (Hex.of_string s)
+  let to_string x = Hex.to_string (`Hex x)
+end
+
+type case = {
+  header_table_size : int option;
+  wire : string;
+  headers : header list;
+}
+
 let parse_file file =
-  match Yojson.Safe.from_file file with
-  | `Assoc l ->
-    begin match List.assoc "cases" l with
-    | `List cases ->
-      List.map begin function
-      | `Assoc l ->
-        begin match List.assoc_opt "header_table_size" l with
-        | Some (`Int size) -> size
-        | _ -> 4096
-        end,
-        begin match List.assoc_opt "wire" l with
-        | Some (`String hex) -> Hex.to_string (`Hex hex)
-        | _ -> ""
-        end,
-        begin match List.assoc "headers" l with
-        | `List headers ->
-          List.map begin function
-          | `Assoc [(name, `String value)] ->
-            {name; value; never_index = false}
-          | _ -> assert false
-          end headers
-        | _ -> assert false
+  Yojson.Basic.from_file file
+  |> Json.member "cases"
+  |> Json.convert_each begin fun case -> {
+      header_table_size = Json.member "header_table_size" case |> Json.to_int_option;
+      wire =
+        begin match Json.member "wire" case |> Json.to_string_option with
+        | Some s -> Hex.to_string s
+        | None -> ""
+        end;
+      headers =
+        Json.member "headers" case
+        |> Json.to_list
+        |> Json.filter_assoc
+        |> List.flatten
+        |> List.map begin fun (name, value) ->
+          {name; value = Json.to_string value; never_index = false}
         end
-      | _ -> assert false
-      end cases
-    | _ -> assert false
-    end
-  | _ -> assert false
+    }
+  end
 
 let encode cases =
   let encoder = Encoder.create 4096 in
-  List.mapi begin fun seq (_, _, headers) ->
-    let t = Faraday.create 0x1000 in
+  cases |> List.map begin fun {headers; _} ->
+    let t = Faraday.create 1200 in
     List.iter (Encoder.encode_header encoder t) headers;
-    let s = Faraday.serialize_to_string t in
-    (seq, Hex.of_string s |> Hex.show, headers)
-  end cases
+    let wire = Faraday.serialize_to_string t in
+    {header_table_size = None; wire; headers}
+  end
 
 let encode_file file =
   let cases = parse_file ("raw-data/" ^ file) in
@@ -46,10 +50,10 @@ let encode_file file =
     Yojson.pretty_to_string @@ `Assoc [
       "description", `String "Encoded by ocaml-hpack";
       "cases", `List (
-        result |> List.map @@ fun (seq, wire, headers) ->
+        result |> List.mapi @@ fun seqno {wire; headers; _} ->
         `Assoc [
-          "seqno", `Int seq;
-          "wire", `String wire;
+          "seqno", `Int seqno;
+          "wire", `String (Hex.of_string wire);
           "headers", `List (
             headers |> List.map @@ fun {name; value; _} ->
             `Assoc [(name, `String value)]
@@ -61,27 +65,30 @@ let encode_file file =
   output_string oc json;
   close_out oc
 
+let encode_files () =
+  prerr_endline "Encode ocaml-hpack";
+  let files = Array.to_list (Sys.readdir "raw-data") in
+  List.iter encode_file files
+
 let header_equal ({name; value; _}, {name = name'; value = value'; _}) =
   name = name' && value = value'
 
 let decode cases =
   let decoder = Decoder.create 65536 in
-  List.iter begin fun (size, s, headers) ->
-    Decoder.set_capacity decoder size;
-    match Angstrom.parse_string (Decoder.headers decoder) s with
+  List.iter begin fun {header_table_size; wire; headers; _} ->
+    begin match header_table_size with
+    | Some capacity -> Decoder.set_capacity decoder capacity
+    | None -> ()
+    end;
+    match Angstrom.parse_string (Decoder.headers decoder) wire with
     | Ok headers' when (List.combine headers headers' |> List.for_all header_equal) -> ()
-    | Ok _ -> failwith (Hex.of_string s |> Hex.show)
+    | Ok _ -> failwith (Hex.of_string wire)
     | Error error -> failwith error
   end cases
 
 let decode_file dir file =
   let cases = parse_file (dir ^ "/" ^ file) in
   decode cases
-
-let encode_files () =
-  prerr_endline "Encode ocaml-hpack";
-  let files = Array.to_list (Sys.readdir "raw-data") in
-  List.iter encode_file files
 
 let decode_files () =
   List.iter begin fun dir ->
@@ -106,7 +113,6 @@ let decode_files () =
   ]
 
 let () =
-  Sys.chdir Sys.argv.(1);
   begin try Unix.mkdir "ocaml-hpack" 0o755
   with Unix.Unix_error (Unix.EEXIST, _, _) -> () end;
   encode_files ();

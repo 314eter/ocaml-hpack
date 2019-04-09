17fe3418010b2d1adc21af80ff390fecc46c9eb5
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2019-04-09 17:26:11 +0200

    Remove capacity from dynamic table

--- src/dynamic_table.ml
+++ src/dynamic_table.ml
@@ -1,9 +1,6 @@
 type t = {
   mutable entries : (string * string) array;
   (* A circular buffer containing name value pairs *)
-  mutable capacity : int;
-  (* The size of the circular buffer, equal to [Array.length entries]
-     The capacity is increased when the buffer is full. *)
   mutable offset : int;
   (* The offset in the circular buffer
      This changes when entries are evicted. *)
@@ -21,18 +18,17 @@ type t = {
 let default_entry = ("", "")
 
 let create ?(on_evict=ignore) max_size =
-  let capacity = 128 in
-  let entries = Array.make capacity default_entry in
-  {entries; capacity; offset = 0; length = 0; size = 0; max_size; on_evict}
+  let entries = Array.make 128 default_entry in
+  {entries; offset = 0; length = 0; size = 0; max_size; on_evict}
 
-let get {entries; capacity; offset; _} i =
-  entries.((offset + i) mod capacity)
+let get {entries; offset; _} i =
+  entries.((offset + i) mod Array.length entries)
 
 let entry_size (name, value) =
   String.length name + String.length value + 32
 
-let evict_one ({entries; capacity; offset; length; on_evict; _} as table) =
-  let i = (offset + length - 1) mod capacity in
+let evict_one ({entries; offset; length; on_evict; _} as table) =
+  let i = (offset + length - 1) mod Array.length entries in
   let entry = entries.(i) in
   entries.(i) <- default_entry;
   table.length <- length - 1;
@@ -45,7 +41,6 @@ let resize table capacity =
       else default_entry
     end in
   table.entries <- entries;
-  table.capacity <- capacity;
   table.offset <- 0
 
 let add table entry =
@@ -54,9 +49,11 @@ let add table entry =
     evict_one table
   done;
   if table.size + entry_size <= table.max_size then begin
-    if table.length = table.capacity then
-      resize table (2 * table.capacity);
-    let offset = (table.offset + table.capacity - 1) mod table.capacity in
+    let capacity = Array.length table.entries in
+    if table.length = capacity then
+      resize table (2 * capacity);
+    let capacity = Array.length table.entries in
+    let offset = (table.offset + capacity - 1) mod capacity in
     table.entries.(offset) <- entry;
     table.offset <- offset;
     table.length <- table.length + 1;
@@ -70,5 +67,5 @@ let change_max_size table max_size =
     evict_one table
   done;
   let capacity = max 128 (max_size lsr 5) in
-  if capacity < table.capacity then
+  if capacity < Array.length table.entries then
     resize table capacity

19ccc2c9eb4662b9e196742246897702d53017d7
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2019-04-13 01:22:22 +0200

    Reorder tests

--- test/test.ml
+++ test/test.ml
@@ -45,6 +45,69 @@ let test entries s = [
   end;
 ]
 
+let test_static =
+  let entries = [
+    Header {name = ":method"; value = "PUT"; never_index = false};
+    Header {name = ":path"; value = "/"; never_index = false};
+    Header {name = ":method"; value = "PUT"; never_index = false};
+  ] in
+  let s =
+    "\x42\x03PUT\
+     \x84\
+     \xbe" in
+  test entries s
+
+let test_dynamic =
+  let entries = [
+    Header {name = "ABC"; value = "ABC"; never_index = false};
+    Header {name = "ABC"; value = "XYZ"; never_index = false};
+    Header {name = "ABC"; value = "ABC"; never_index = false};
+  ] in
+  let s =
+    "\x40\x03ABC\x03ABC\
+     \x7e\x03XYZ\
+     \xbf" in
+  test entries s
+
+let test_huffman =
+  let entries = [
+    Header {name = "abc"; value = "012"; never_index = false};
+  ] in
+  let s = "\x40\x82\x1c\x64\x82\x00\x45" in
+  test entries s
+
+let test_no_index =
+  let entries = [
+    Header {name = "content-length"; value = "350"; never_index = false};
+    Header {name = "content-length"; value = "350"; never_index = false};
+  ] in
+  let s =
+    "\x0f\x0d\x03350\
+     \x0f\x0d\x03350" in
+  test entries s
+
+let test_never_index =
+  let entries = [
+    Header {name = "ABC"; value = "XYZ"; never_index = true};
+    Header {name = "ABC"; value = "XYZ"; never_index = true};
+  ] in
+  let s =
+    "\x10\x03ABC\x03XYZ\
+     \x10\x03ABC\x03XYZ" in
+  test entries s
+
+let test_size_update =
+  let entries = [
+    Header {name = "ABC"; value = "XYZ"; never_index = false};
+    Size 0; Size 100;
+    Header {name = "ABC"; value = "XYZ"; never_index = false};
+  ] in
+  let s =
+    "\x40\x03ABC\x03XYZ\
+     \x20\x3f\x45\
+     \x40\x03ABC\x03XYZ" in
+  test entries s
+
 let test_eviction =
   let entries = [
     Size 38;
@@ -63,74 +126,11 @@ let test_eviction =
      \x40\x04ABCD\x03XYZ" in
   test entries s
 
-let test_size_update =
-  let entries = [
-    Header {name = "ABC"; value = "XYZ"; never_index = false};
-    Size 0; Size 100;
-    Header {name = "ABC"; value = "XYZ"; never_index = false};
-  ] in
-  let s =
-    "\x40\x03ABC\x03XYZ\
-     \x20\x3f\x45\
-     \x40\x03ABC\x03XYZ" in
-  test entries s
-
-let test_never_index =
-  let entries = [
-    Header {name = "ABC"; value = "XYZ"; never_index = true};
-    Header {name = "ABC"; value = "XYZ"; never_index = true};
-  ] in
-  let s =
-    "\x10\x03ABC\x03XYZ\
-     \x10\x03ABC\x03XYZ" in
-  test entries s
-
-let test_huffman =
-  let entries = [
-    Header {name = "abc"; value = "012"; never_index = false};
-  ] in
-  let s = "\x40\x82\x1c\x64\x82\x00\x45" in
-  test entries s
-
-let test_dynamic =
-  let entries = [
-    Header {name = "ABC"; value = "ABC"; never_index = false};
-    Header {name = "ABC"; value = "XYZ"; never_index = false};
-    Header {name = "ABC"; value = "ABC"; never_index = false};
-  ] in
-  let s =
-    "\x40\x03ABC\x03ABC\
-     \x7e\x03XYZ\
-     \xbf" in
-  test entries s
-
-let test_static =
-  let entries = [
-    Header {name = ":method"; value = "PUT"; never_index = false};
-    Header {name = ":path"; value = "/"; never_index = false};
-    Header {name = ":method"; value = "PUT"; never_index = false};
-  ] in
-  let s =
-    "\x42\x03PUT\
-     \x84\
-     \xbe" in
-  test entries s
-
-let test_no_index =
-  let entries = [
-    Header {name = "content-length"; value = "350"; never_index = false};
-    Header {name = "content-length"; value = "350"; never_index = false};
-  ] in
-  let s =
-    "\x0f\x0d\x03350\
-     \x0f\x0d\x03350" in
-  test entries s
-
 let () =
   Alcotest.run "Hpack" [
-    "Huffman Encoding", test_huffman; (* RFC7541§5.2 *)
     "Static Indexing", test_static; (* RFC7541§2.3.1 *)
     "Dynamic Indexing", test_dynamic; (* RFC7541§2.3.2 *)
+    "Huffman Encoding", test_huffman; (* RFC7541§5.2 *)
     "No Indexing", test_no_index; (* RFC7541§6.2.2 *)
     "Never Index", test_never_index; (* RFC7541§7.1.3 *)
     "Table Size Update", test_size_update; (* RFC7541§4.2 *)

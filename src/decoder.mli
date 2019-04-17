a61a6f8cc116c959ac1ef394390447aeca4802f8
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2019-04-17 22:49:11 +0200

    Add max_field_size (#7)

--- src/decoder.mli
+++ src/decoder.mli
@@ -2,10 +2,14 @@ open S
 
 type t
 
-val create : ?max_size_limit:int -> unit -> t
+val create : ?max_size_limit:int -> ?max_field_size:int -> unit -> t
 (** [create ~max_size_limit ()] intializes a decoder with a dynamic table with
-    maximum size [max_size_limit]. The default is 4096. Each different
-    should be communicated to the encoder by the protocol. *)
+    maximum size [max_size_limit]. The default is 4096. Each other value should
+    be communicated to the encoder by the protocol.
+
+    [max_field_size] is the maximum allowed size in bytes of one name or value
+    field. The default is 4096.
+*)
 
 val header : t -> header Angstrom.t
 (** A parser for one header *)

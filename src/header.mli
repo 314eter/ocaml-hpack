f2bf7a907a644896dcd5d749559e401b595780a9
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2019-04-24 14:40:59 +0200

    Add documentation

--- src/header.mli
+++ src/header.mli
@@ -5,9 +5,16 @@ type t = private {
 }
 
 val make : ?never_index:bool -> string -> string -> t
+(** [make name value] creates a new header field. The optional argument
+    [never_index] can be used for sensitive values that should not be kept in
+    memory.
+*)
 
 val name : t -> string
+(** Extracts the name of a header field. *)
 
 val value : t -> string
+(** Extracts the value of a header field. *)
 
 val never_index : t -> bool
+(** [never_index header] is true if this header field will never be indexed. *)
